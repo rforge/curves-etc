5c9f38bece2aecee53e78cbc89d10f10e836e1a6
maechler <maechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2001-08-18 16:27:52 +0000

    version of 2001.08.18.16.27.52 (now is 27 Jul 2010).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lokern/R/varNPreg.R
+++ pkg/lokern/R/varNPreg.R
@@ -9,29 +9,29 @@
 
 varest <- function(x,y)
 {
-  ## Purpose: Nonparametric Leave-1-out Residuals and Variance Estimator
-  ##	in the model   y[i] = mu(x(i)) + E[i] ,  E[i] ~ (0, sigma^2), i.i.d
-  ## -------------------------------------------------------------------------
-  ## Arguments: (x,y)
-  ## -------------------------------------------------------------------------
-##     subroutine resest(t,x,n, res,snr,sigma2)
-##-----------------------------------------------------------------------
-## purpose:
-##
-##       computes one-leave-out residuals for nonparametric estimation
-##       of residual variance (local linear approximation followed by
-##       reweighting)
-##
-## parameters:
-##
-##     input   t(n)      abscissae (ordered: t(i) <= t(i+1))
-##     input   x(n)      data
-##     input   n         length of data ( >2 )
-##     output  res(n)    residuals at t(1),...,t(n)
-##     output  snr       explained variance of the true curve
-##     output  sigma2    estimation of sigma^2 (residual variance)
+    ## Purpose: Nonparametric Leave-1-out Residuals and Variance Estimator
+    ##	in the model   y[i] = mu(x(i)) + E[i] ,  E[i] ~ (0, sigma^2), i.i.d
+    ## -------------------------------------------------------------------------
+    ## Arguments: (x,y)
+    ## -------------------------------------------------------------------------
+    ##     subroutine resest(t,x,n, res,snr,sigma2)
+    ##-----------------------------------------------------------------------
+    ## purpose:
+    ##
+    ##       computes one-leave-out residuals for nonparametric estimation
+    ##       of residual variance (local linear approximation followed by
+    ##       reweighting)
+    ##
+    ## parameters:
+    ##
+    ##     input   t(n)      abscissae (ordered: t(i) <= t(i+1))
+    ##     input   x(n)      data
+    ##     input   n         length of data ( >2 )
+    ##     output  res(n)    residuals at t(1),...,t(n)
+    ##     output  snr       explained variance of the true curve
+    ##     output  sigma2    estimation of sigma^2 (residual variance)
 
-  ## Author: Martin Maechler, Date:  9 Jul 2001, 14:47
+    ## Author: Martin Maechler, Date:  9 Jul 2001, 14:47
     if(2 >= n <- length(x)) stop("n := length(x)  must be at least 3")
     if(is.unsorted(x)) stop("`x' must be ordered increasingly")
     if(n != length(y)) stop("`x' and `y' must have same length")
