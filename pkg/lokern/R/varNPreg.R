754c05c74a7105f717a302316c4f51dccb0d37fe
maechler <maechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2001-08-20 18:17:19 +0000

    version of 2001.08.20.18.17.19 (now is 27 Jul 2010).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lokern/R/varNPreg.R
+++ pkg/lokern/R/varNPreg.R
@@ -1,4 +1,4 @@
-#### varest.R : Nonparametric Variance Estimator
+#### varNPreg.R : Nonparametric Variance Estimator
 ####
 #### S/R interface to the resest() Fortran subroutine
 
@@ -7,29 +7,10 @@
 #### PUBLIC LICENSE Version 2, June 1991, see the COPYING file from R,
 #### or http://www.gnu.org/copyleft/gpl.html
 
-varest <- function(x,y)
+varNPreg <- function(x,y)
 {
     ## Purpose: Nonparametric Leave-1-out Residuals and Variance Estimator
     ##	in the model   y[i] = mu(x(i)) + E[i] ,  E[i] ~ (0, sigma^2), i.i.d
-    ## -------------------------------------------------------------------------
-    ## Arguments: (x,y)
-    ## -------------------------------------------------------------------------
-    ##     subroutine resest(t,x,n, res,snr,sigma2)
-    ##-----------------------------------------------------------------------
-    ## purpose:
-    ##
-    ##       computes one-leave-out residuals for nonparametric estimation
-    ##       of residual variance (local linear approximation followed by
-    ##       reweighting)
-    ##
-    ## parameters:
-    ##
-    ##     input   t(n)      abscissae (ordered: t(i) <= t(i+1))
-    ##     input   x(n)      data
-    ##     input   n         length of data ( >2 )
-    ##     output  res(n)    residuals at t(1),...,t(n)
-    ##     output  snr       explained variance of the true curve
-    ##     output  sigma2    estimation of sigma^2 (residual variance)
 
     ## Author: Martin Maechler, Date:  9 Jul 2001, 14:47
     if(2 >= (n <- length(x))) stop("n := length(x)  must be at least 3")
