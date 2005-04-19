e774de4103f4317aa61946a59e6f2d6127a1654f
maechler <maechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-04-19 10:40:53 +0000

    version of 2005.04.19.10.40.53 (now is 28 Jul 2010).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@122 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lpridge/R/lpridge.R
+++ pkg/lpridge/R/lpridge.R
@@ -1,119 +1,126 @@
 ## lpridge	local polynomial regression fitting with ridging
 
 lpridge <- function(x,y,bandwidth,
-                 deriv=0, n.out=200,x.out=NULL, order=NULL,
-                 ridge=NULL, weight="epa", mnew=100, var=FALSE)
+		 deriv = 0, n.out = 200,x.out = NULL, order = NULL,
+		 ridge = NULL, weight = "epa", mnew = 100, var = FALSE)
 {
-  ## x		inputgrid
-  ## y		data
+    ## x		inputgrid
+    ## y		data
 
-  ## control and sort inputgrid and data
-  n <- length(x)
-  if (length(y)!=n) stop("Input grid and data must have the same length.")
-  sorvec <- sort.list(x)
-  x <- x[sorvec]
-  y <- y[sorvec]
+    ## control and sort inputgrid and data
+    n <- length(x)
+    if (length(y) != n) stop("Input grid and data must have the same length.")
+    sorvec <- sort.list(x)
+    x <- x[sorvec]
+    y <- y[sorvec]
 
-  ## bandwidth	bandwidth for estimation
+    ## bandwidth	bandwidth for estimation
+    ## deriv	derivative of regression function to be estimated
+    ## n.out	length of outputgrid
+    ## x.out	outputgrid
 
-  ## deriv	derivative of regression function to be estimated
+    ## compute and control outputgrid
+    if (is.null(x.out)) {
+	n.out <- as.integer(n.out)
+	x.out <- seq(min(x),max(x),length = n.out)
+    }
+    else {
+	n.out <- length(x.out)
+    }
 
-  ## n.out	length of outputgrid
-  ## x.out	outputgrid
+    ## compute vector of bandwiths
+    if (length(bandwidth) == 1)
+	bandwidth <- rep(bandwidth,n.out)
+    else if (length(bandwidth) != n.out)
+	stop("Length of bandwith is not equal to length of output grid.")
 
-  ## compute and control outputgrid
-  if (is.null(x.out)) {
-    n.out <- as.integer(n.out)
-    x.out <- seq(min(x),max(x),length=n.out)
-  }
-  else {
-    n.out <- length(x.out)
-  }
+    ## sort outputgrid and bandwidth
+    sorvec <- sort.list(x.out)
+    x.out <- x.out[sorvec]
+    bandwidth <- bandwidth[sorvec]
 
-  ## compute vector of bandwiths
-  if (length(bandwidth)==1)
-    bandwidth <- rep(bandwidth,n.out)
-  else if (length(bandwidth) != n.out)
-    stop("Length of bandwith is not equal to length of output grid.")
+    ## order	order of local polynomial approximation
+    ## check order, deriv
+    if (is.null(order)) order <- deriv+1
+    if (order < 0) stop("Polynomial order is negative.")
+    if (deriv < 0) stop("Order of derivative is negative.")
+    if (deriv > order)
+	stop("Order of derivative is larger than polynomial order.")
 
-  ## sort outputgrid and bandwidth
-  sorvec <- sort.list(x.out)
-  x.out <- x.out[sorvec]
-  bandwidth <- bandwidth[sorvec]
+    ## ridge	ridge parameter
+    if (is.null(ridge)) {
+	ridge <- 5*sqrt(length(x)*mean(bandwidth)/diff(range(x)))*
+	    mean(bandwidth)^(2*deriv)/((2*deriv+3)*(2*deriv+5))
+	if (order == deriv) ridge <- 0
+    }
+    if (order == deriv & ridge > 0)
+	stop("ridging is impossible for order==deriv.")
 
-  ## order	order of local polynomial approximation
-  ## check order, deriv
-  if (is.null(order)) order <- deriv+1
-  if (order<0) stop("Polynomial order is negative.")
-  if (deriv<0) stop("Order of derivative is negative.")
-  if (deriv>order) stop("Order of derivative is larger than polynomial order.")
+    ## weight	kernel weights
+    if (weight == "epa") {
+	kord <- 2
+	wk <- c(1,0,-1)
+    }
+    else if (weight == "bi") {
+	kord <- 4
+	wk <- c(1,0,-2,0,1)
+    }
+    else if (weight == "tri") {
+	kord <- 6
+	wk <- c(1,0,-3,0,3,0,-1)
+    }
+    else if (is.numeric(weight)) {
+	kord <- length(weight)-1
+	wk <- weight
+    }
+    else stop("Error in weight.")
 
-  ## ridge	ridge parameter
-  if (is.null(ridge)) {
-    ridge <- 5*sqrt(length(x)*mean(bandwidth)/diff(range(x)))*
-      mean(bandwidth)^(2*deriv)/((2*deriv+3)*(2*deriv+5))
-    if (order==deriv) ridge <- 0 }
-  if (order==deriv & ridge>0) stop("ridging is impossible for order==deriv.")
-  ## weight	kernel weights
-  if (weight=="epa") {
-    kord <- 2
-    wk <- c(1,0,-1) }
-  else if (weight=="bi") {
-    kord <- 4
-    wk <- c(1,0,-2,0,1) }
-  else if (weight=="tri") {
-    kord <- 6
-    wk <- c(1,0,-3,0,3,0,-1) }
-  else if (is.numeric(weight)) {
-    kord <- length(weight)-1
-    wk <- weight }
-  else stop("Error in weight.")
+    ## mnew		force of restart
 
-  ## mnew		force of restart
+    ## var		switch for variance estimation
+    var <- as.logical(var)
 
-  ## var		switch for variance estimation
-  var <- as.logical(var)
+    ## internal parameters and arrays (see fortran routine)
+    leng <- 10
+    nmoms <- as.integer(length(x)/leng+1)
+    imoms <- integer(nmoms)
+    moms <- double(nmoms*4*(order+max(2,kord)+as.integer(var)))
 
-  ## internal parameters and arrays (see fortran routine)
-  leng <- 10
-  nmoms <- as.integer(length(x)/leng+1)
-  imoms <- integer(nmoms)
-  moms <- double(nmoms*4*(order+max(2,kord)+as.integer(var)))
+    ## check internal limitations from fortran routine
+    if (order > 10)
+	stop("polynomial order exceeds 10.")
+    if ((kord+order) > 12)
+	stop("Order of kernel weights + polynomial order exceeds 12.")
 
-  ## check internal limitations from fortran routine
-  if (order>10)
-    stop("polynomial order exceeds 10.")
-  if ((kord+order)>12)
-    stop("Order of kernel weights + polynomial order exceeds 12.")
+    res <- .Fortran("lpridge",
+		    x = as.double(x),
+		    y = as.double(y),
+		    as.integer(n),
+		    bandwidth = as.double(bandwidth),
+		    deriv = as.integer(deriv),
+		    order = as.integer(order),
+		    kord = as.integer(kord),
+		    wk = as.double(wk),
+		    x.out = as.double(x.out),
+		    as.integer(n.out),
+		    mnew = as.integer(mnew),
+		    as.integer(imoms),
+		    as.double(moms),
+		    est = double(n.out),
+		    as.integer(leng),
+		    as.integer(nmoms),
+		    var = as.integer(var),
+		    est.var = double(n.out),
+		    ridge = as.double(ridge),
+		    nsins = integer(1),
+		    PACKAGE = "lpridge")
 
-  res <- .Fortran("lpridge",
-                  x=as.double(x),
-                  y=as.double(y),
-                  as.integer(n),
-                  bandwidth=as.double(bandwidth),
-                  deriv=as.integer(deriv),
-                  order=as.integer(order),
-                  kord=as.integer(kord),
-                  wk=as.double(wk),
-                  x.out=as.double(x.out),
-                  as.integer(n.out),
-                  mnew=as.integer(mnew),
-                  as.integer(imoms),
-                  as.double(moms),
-                  est=double(n.out),
-                  as.integer(leng),
-                  as.integer(nmoms),
-                  var=as.integer(var),
-                  est.var=double(n.out),
-                  ridge=as.double(ridge),
-                  nsins=integer(1),
-                  PACKAGE = "lpridge")
+    if (res$nsins > 0)
+	warning(res$nsins,
+		" singularity exceptions. Corresponding estimators set zero.")
 
-  if (res$nsins>0) warning(paste(res$nsins,"singularity exceptions. ",
-                                 "Corresponding estimators set zero."))
-
-  return(x=x,y=y,bandwidth=res$bandwidth,deriv=deriv,x.out=x.out,
-         order=order,ridge=ridge,weight=wk,mnew=mnew,
-         var=var,est=res$est,est.var=res$est.var)
+    list(x = x, y = y, bandwidth = res$bandwidth, deriv = deriv, x.out = x.out,
+	 order = order, ridge = ridge, weight = wk, mnew = mnew,
+	 var = var, est = res$est, est.var = res$est.var)
 }
 
