b5939949ec31e8994b752b4c24c1defb05760bd9
maechler <maechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-31 14:15:27 +0000

    Initial revision
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@155 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nor1mix/tests/MW.R
@@ -0,0 +1,21 @@
+### Since we had an undetected bug in  rnorMix()...
+
+## These are defined as norMix() calls in  ../R/zMarrWand-dens.R
+library("nor1mix")
+ppos <- which("package:nor1mix" == search())
+nms <- ls(pat="^MW.nm", pos = ppos)
+nms <- nms[order(as.numeric(substring(nms,6)))] # warning <== "MW.nm2.old"
+
+set.seed(123)
+for(n in nms) {
+    obj <- get(n, pos = ppos)
+    cat("\n",n,":\n"); print(obj)
+    cat("4 random X from", n,":")
+    print(rnorMix(4, obj))
+}
+
+stopifnot(sapply(nms, function(n) {
+    o <- get(n)
+    S <- !is.unsorted(o[,"mu"])
+    !S || (S && identical(o, sort(o)))
+}))
