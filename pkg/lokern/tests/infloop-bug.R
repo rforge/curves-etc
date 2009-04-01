883d61eee25684be11df67668b02cde3dbdab405
maechler <maechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-01 22:57:49 +0000

    version of 2009.04.01.22.57.49 (now is 27 Jul 2010).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lokern/tests/infloop-bug.R
+++ pkg/lokern/tests/infloop-bug.R
@@ -1,39 +1,34 @@
-## From: Ravi Varadhan <rvaradhan@jhmi.edu>
-## To: maechler@stat.math.ethz.ch
-## Subject: A potential "bug" report - An issue with "lokern" package
-## Date: Tue, 03 Mar 2009 12:43:36 -0500
-
-## Dear Martin,
-
-## I was trying to run the glkerns() and lokerns() functions in the "lokern" package for the Old Faithful Geyser data from "MASS".  Both functions just freeze-up.  I think that the problem lies in "x" values being not unique for this data (some values are replicated).
-
-## However, if run with is.rand = FALSE, then everything is ok.  It would useful, I think, if you have a check in the code that checks for dupicity in X-values and then either provides a warning to change the "is.rand" option or just go ahead and change that inside the code.
-
-data(geyser, package = "MASS")
-
-x <- geyser$duration
-y <- geyser$waiting
-plot(x, y)
+### One of the examples that gave an infinite loop:
 
 require(lokern)
-ord <- order(x)
-x <- x[ord]
-y <- y[ord]
-glkerns(x, y, is.rand=FALSE) # is fine
-glkerns(x, y) # freezes-up
 
+ep <- 2e-6
+x <- c(rep(118 + ep, 2), 150, 207, 240, 240, 240,
+       263-ep, 270, 275-ep, 290-ep)
+##x <- c(rep(-32+ep, 2), 0, 57, 90, 90, 90, 113-ep, 120, 125-ep, 140-ep)
+y <- c(19, 27, 23, 27, 4, 15, 21, 33, 5, 15, 0)
 
-## Thank you,
-## Ravi.
+plot(x,y)
 
-## ____________________________________________________________________
+g3  <- glkerns(x, y) # used to "freeze up" , i.e. Infinite loop --- now fine
+lok <- lokerns(x, y) # ditto
 
-## Ravi Varadhan, Ph.D.
-## Assistant Professor,
-## Division of Geriatric Medicine and Gerontology
-## School of Medicine
-## Johns Hopkins University
+with(g3,  lines(est ~ x.out, col = 2))
+with(lok, lines(est ~ x.out, col = 3, lwd=2))
 
-## Ph. (410) 502-2619
-## email: rvaradhan@jhmi.edu
+g.e <- c(26.370, 26.088, 25.807, 25.526, 25.244, 24.963, 24.682, 24.400, 24.119,
+         23.838, 23.556, 23.275, 22.994, 22.712, 22.431, 22.109, 21.552, 20.801,
+         20.050, 19.299, 18.548, 17.797, 17.046, 16.296, 15.545, 14.794, 14.043,
+         13.292, 12.541, 11.790)
+l.e <- c(26.407, 26.120, 25.831, 25.542, 25.253, 24.966, 24.681, 24.399, 24.122,
+         23.849, 23.580, 23.315, 23.054, 22.796, 22.537, 22.246, 21.997, 21.669,
+         21.204, 20.523, 19.614, 18.656, 17.659, 16.631, 15.579, 14.508, 13.421,
+         12.324, 11.221, 10.115)
+ii <- 1+10*(0:29)
+stopifnot(all.equal( g3$est[ii], g.e, tol = 4e-5),
+          all.equal(lok$est[ii], l.e, tol = 4e-5))
+
+if(!interactive()) warnings()
+
+cat('Time elapsed: ', proc.time(),'\n') # "stats"
 
