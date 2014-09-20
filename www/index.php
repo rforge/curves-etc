caae5ebdf419a67c449e05f18ed2748879fd02da
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-20 10:48:00 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@290 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -42,10 +42,32 @@ fclose($handle);
 echo $contents; } ?>
 
 <!-- end of project description -->
+<p>
+This is the Subversion repository of <strong>several</strong> <a href="http://www.r-project.org">R</a> packages, all related to <em>curve estimation</em> aka curve <em>"fitting"</em>.
+</p>
 
-<p> No content added. </p>
+<ul>
+<li> The individual packages' CRAN pages are
+<p>
+<table border="2" cellspacing="0" cellpadding="6" rules="groups" frame="hsides">
+<thead>
+<tr><th scope="col" class="left">Name</th><th scope="col" class="left">CRAN page</th><th scope="col" class="left">Short Description</th></tr>
+</thead>
+<tbody>
+<tr><td>cobs</td><td><a href="http://cran.r-project.org/web/packages/cobs">http://cran.r-project.org/web/packages/cobs</a></td><td>Constrained B-Splines</td></tr>
+<tr><td>cobs99</td><td><a href="http://cran.r-project.org/web/packages/cobs99">http://cran.r-project.org/web/packages/cobs99</a></td><td>COBS - old 1999 version</td></tr>
+<tr><td>lokern</td><td><a href="http://cran.r-project.org/web/packages/lokern">http://cran.r-project.org/web/packages/lokern</a></td><td>Kernel Regression Smoothing, Local/Global Bandwidth</td></tr>
+<tr><td>lpridge</td><td><a href="http://cran.r-project.org/web/packages/lpridge">http://cran.r-project.org/web/packages/lpridge</a></td><td>Local Polynomial (Ridge) Regression</td></tr>
+<tr><td>nor1mix</td><td><a href="http://cran.r-project.org/web/packages/nor1mix">http://cran.r-project.org/web/packages/nor1mix</a></td><td>Normal (1-d) Mixture Models</td></tr>
+</tbody>
+</table></p>
+</li>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<li> The <strong>R-forge project summary page</strong> you can
+  find <a href="http://<?php echo $domain; ?>/projects/<?php echo
+		$group_name; ?>/"><strong>here</strong></a>.
+</li>
+</ul>
 
 </body>
 </html>
