fc0c74fa133db0c34c84408d578095edbbfa271b
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-09-07 20:42:23 +0000

    use modern do - loops (not with labels)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@316 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lpridge/src/lpepa.f
+++ pkg/lpridge/src/lpepa.f
@@ -111,8 +111,9 @@ c - maximum order of moments
       na=p+1
 c - nuefak = faktorial of nue
       nuefak=1d0
-      do 20 i=1,nue
-20       nuefak=nuefak*i
+      do i=1,nue
+         nuefak=nuefak*i
+      end do
 c - binomial coefficients
       do kk=0,powmax
          bin(kk,0)=1
@@ -287,9 +288,11 @@ c     - calculate Tn,l
      +              (tti*tti*w(l,2) - 2d0*tti*w(l+1,2) + w(l+2,2))
             enddo
 c     - construct matrix
-            do 360 j=0,p
-               do 360 l=1,j+1
- 360              w1(j*na+l)=w(l+j-1,4)
+            do j=0,p
+               do l=1,j+1
+                  w1(j*na+l)=w(l+j-1,4)
+               end do
+            end do
             irec=1
             goto 500
          endif
@@ -297,12 +300,15 @@ c  -- else -- (but we jump here from outside, hence "need" goto 500 above)
 c     ----> unweighted LSE
 
 c     - store t_n,l
- 400     do 420 l=0,p
- 420        w(l,5)=w(l,2)
+ 400     do l=0,p
+            w(l,5)=w(l,2)
+         end do
 c     - construct matrix
-         do 430 j=0,p
-            do 430 l=1,j+1
- 430           w1(j*na+l)=w(l+j-1,1)
+         do j=0,p
+            do l=1,j+1
+               w1(j*na+l)=w(l+j-1,1)
+            end do
+         end do
          iup=mnew
          irec=2
 c  -- endif
@@ -359,25 +365,29 @@ c
 c - compute variance
 c
          if (nvar.le.0) goto 990
-         do 710 j=0,nue
-710         w(j,3)=0d0
+         do j=0,nue
+            w(j,3)=0d0
+         end do
          ttti=1d0
-         do 740 j=nue,p
+         do j=nue,p
             w(j,3)=bin(j,nue)*ttti*nuefak
-740         ttti=ttti*tti
+            ttti=ttti*tti
+         end do
          call lpsv(w1,work,w(0,3),na, zer,na)
          if (irec.ne.2) then
 c     ----> weighted LSE
             bb=1d0/(b(i)*b(i))
-            do 810 l=0,2*p
- 810           w(l,4)=w(l,1)
+            do l=0,2*p
+               w(l,4)=w(l,1)
      .           -2d0*bb*(tti*tti*w(l,1)-2d0*tti*w(l+1,1)+w(l+2,1))
      .           +bb*bb*(tti*tti*tti*tti*w(l,1)-4d0*tti*tti*tti*w(l+1,1)
-     .           +6d0*tti*tti*w(l+2,1)-4d0*tti*w(l+3,1)+w(l+4,1))
+     .              +6d0*tti*tti*w(l+2,1)-4d0*tti*w(l+3,1)+w(l+4,1))
+            end do
          else
 c     ----> unweighted LSE
-            do 860 l=0,2*p
- 860           w(l,4)=w(l,1)
+            do l=0,2*p
+               w(l,4)=w(l,1)
+            end do
 c     - variance (sigma = 1)
 c     - var = u'* (X'W X)**-1 X'W**2 X (X'W X)**-1 * u
          endif
