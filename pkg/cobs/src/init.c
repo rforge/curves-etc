9dd5a1ef48f092b241e2cc150cf70653c5c228f5
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-23 08:43:53 +0000

    SR_R is not yet part of the released package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@339 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cobs/src/init.c
+++ pkg/cobs/src/init.c
@@ -2,26 +2,13 @@
 #include <R_ext/Rdynload.h>
 
 #include "splines.h"
-/*
-  The following symbols/expresssions for .NAME have been omitted
-
-    spline_basis
-    spline_value
-
-  Most likely possible values need to be added below.
-*/
-
-/* FIXME:
-   Check these declarations against the C/Fortran source code.
-*/
-
-/* .C calls */
-extern void SR_R(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+/* other .C calls */
+/* extern void SR_R(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *); */
 
 static const R_CMethodDef CEntries[] = {
     {"spline_basis", (DL_FUNC) &spline_basis, 8},
     {"spline_value", (DL_FUNC) &spline_value, 8},
-    {"SR_R", (DL_FUNC) &SR_R, 16},
+    /* {"SR_R", (DL_FUNC) &SR_R, 16}, */
     {NULL, NULL, 0}
 };
 
