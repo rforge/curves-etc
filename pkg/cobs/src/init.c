9b73513bff15f09f56ee8ef688ced2ec8bbac9a7
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-24 13:23:18 +0000

    final (?!) tweak for C symbol registration
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@340 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cobs/src/init.c
+++ pkg/cobs/src/init.c
@@ -1,13 +1,32 @@
 #include <stdlib.h> // for NULL
+
+#include <R.h>
+#include <Rinternals.h>
+
 #include <R_ext/Rdynload.h>
 
 #include "splines.h"
+
+#define CDEF(name)  {#name, (DL_FUNC) &name, sizeof(name ## _t)/sizeof(name ## _t[0]), name ##_t}
+
+#define CALLDEF(name, n)  {#name, (DL_FUNC) &name, n}
+
 /* other .C calls */
 /* extern void SR_R(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *); */
 
+// ./splines.c :
+static R_NativePrimitiveArgType spline_basis_t[] = {
+    REALSXP, INTSXP, INTSXP,
+    REALSXP, INTSXP, INTSXP,
+    REALSXP, INTSXP };
+
+static R_NativePrimitiveArgType spline_value_t[] = {
+    REALSXP, REALSXP, INTSXP, INTSXP,
+    REALSXP, INTSXP, INTSXP, REALSXP };
+
 static const R_CMethodDef CEntries[] = {
-    {"spline_basis", (DL_FUNC) &spline_basis, 8},
-    {"spline_value", (DL_FUNC) &spline_value, 8},
+    CDEF(spline_basis),
+    CDEF(spline_value),
     /* {"SR_R", (DL_FUNC) &SR_R, 16}, */
     {NULL, NULL, 0}
 };
