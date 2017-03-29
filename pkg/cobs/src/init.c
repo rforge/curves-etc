494f10a50abeb124f1563c5a77581c73a6ff7f44
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-29 07:59:46 +0000

    add "SR" properly, as conreg(*, method="SR"); incl. a verbose (in C code)!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/curves-etc@341 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cobs/src/init.c
+++ pkg/cobs/src/init.c
@@ -6,13 +6,11 @@
 #include <R_ext/Rdynload.h>
 
 #include "splines.h"
+#include "SR.h"
 
 #define CDEF(name)  {#name, (DL_FUNC) &name, sizeof(name ## _t)/sizeof(name ## _t[0]), name ##_t}
 
 #define CALLDEF(name, n)  {#name, (DL_FUNC) &name, n}
-
-/* other .C calls */
-/* extern void SR_R(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *); */
 
 // ./splines.c :
 static R_NativePrimitiveArgType spline_basis_t[] = {
@@ -24,10 +22,18 @@ static R_NativePrimitiveArgType spline_value_t[] = {
     REALSXP, REALSXP, INTSXP, INTSXP,
     REALSXP, INTSXP, INTSXP, REALSXP };
 
+static R_NativePrimitiveArgType SR_R_t[] = {
+    INTSXP, REALSXP, INTSXP, INTSXP,
+    REALSXP, REALSXP,
+    REALSXP, REALSXP, REALSXP,
+    REALSXP, REALSXP, REALSXP,
+    REALSXP, INTSXP, INTSXP,
+    REALSXP, INTSXP };
+
 static const R_CMethodDef CEntries[] = {
     CDEF(spline_basis),
     CDEF(spline_value),
-    /* {"SR_R", (DL_FUNC) &SR_R, 16}, */
+    CDEF(SR_R),
     {NULL, NULL, 0}
 };
 
--- pkg/cobs/tests/wind.R
+++ pkg/cobs/tests/wind.R
@@ -1,6 +1,9 @@
 suppressMessages(library(cobs))
 
 source(system.file("util.R", package = "cobs"))
+(doExtra <- doExtras())
+source(system.file("test-tools-1.R", package="Matrix", mustWork=TRUE))
+showProc.time() # timing here (to be faster by default)
 
 data(DublinWind)
 attach(DublinWind)##-> speed & day (instead of "wind.x" & "DUB.")
@@ -15,6 +18,8 @@ co50.1 <- cobs(day, speed, constraint= "periodic", tau= .5, lambda= 2.2,
 co50.2 <- cobs(day, speed, constraint= "periodic", tau= .5, lambda= 2.2,
                degree = 2)
 
+showProc.time()
+
 plot(day,speed)
 lines(day[iday], fitted(co50.1)[iday], col="orange", lwd = 2)
 lines(day[iday], fitted(co50.2)[iday], col="light blue", lwd = 2)
@@ -28,6 +33,8 @@ co.o50 <-
       tau = .5, lambda = -1, method = "uniform")
 summary(co.o50)
 
+showProc.time()
+
 op <- par(mfrow = c(3,1), mgp = c(1.5, 0.6,0), mar=.1 + c(3,3:1))
 with(co.o50, plot(pp.sic ~ pp.lambda, type ="o",
                   col=2, log = "x", main = "co.o50: periodic"))
@@ -39,6 +46,8 @@ with(co.o50, plot(pp.sic - of ~ pp.lambda, type ="o", ylim = c(6e-15, 8e-15),
                   col=2, log = "x", main = "co.o50: periodic"))
 par(op)
 
+showProc.time()
+
 ## cobs99: Since SIC chooses a lambda that corresponds to the smoothest
 ## possible fit, rerun cobs with a larger lstart value
 ## (lstart <- log(.Machine$double.xmax)^3) # 3.57 e9
@@ -48,6 +57,8 @@ co.o50. <-
          tau = .5,lambda = -1, method = "quantile")
 summary(co.o50.)
 (pc.5 <- predict(co.o50., interval = "both"))
+
+showProc.time()
 
 co.o50.. <- cobs(day,speed, knots.add = TRUE, repeat.delete.add=TRUE,
                  constraint = "periodic", nknots = 10,
@@ -59,11 +70,15 @@ co.o9 <- ## Compute the .9 quantile smoothing B-spline
 summary(co.o9)
 (pc.9 <- predict(co.o9,interval = "both"))
 
+showProc.time()
+
 co.o1 <- ## Compute the .1 quantile smoothing B-spline
     cobs(day,speed,knots.add = TRUE, constraint = "periodic",nknots = nknots,
          tau = .1,lambda = -1, method = "uniform")
 summary(co.o1)
 (pc.1 <- predict(co.o1, interval = "both"))
+
+showProc.time()
 
 par(mfrow = c(1,2), mgp = c(1.5, .6,0), mar = .1 + c(3,3,1,1))
 plot(day,speed, pch = 3, cex=0.6,  xlab = "DAYS", ylab = "SPEED (knots)")
@@ -76,3 +91,5 @@ lines(pc.9, col = 3)
 lines(pc.1, col = 3)
 abline(v = co.o50.$knots, lty = 3, col = "gray70")
 ## rather rug(co.o5$knots, lty = 2)
+
+showProc.time()
